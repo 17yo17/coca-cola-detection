a40daa3f869381f43db82d3a946f89335d80704f
17yo17 <117047772+17yo17@users.noreply.github.com>
2024-04-12 11:59:43 -0700

    Update coca-cola-detection.py

--- code/coca-cola-detection.py
+++ code/coca-cola-detection.py
@@ -3,15 +3,15 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")
 model = YOLO("yolov8n.pt")
 
-data_path = "/Users/taonoryo/Library/Mobile Documents/com~apple~CloudDocs/Documents/sdsu/cs577/ultralytics/ultralytics/Coca-cola-Detection-1/data.yaml"
+data_path = "/ultralytics/Coca-cola-Detection-1/data.yaml"
 
 model.train(data=data_path, epochs=3)
 
-model_path="/Users/taonoryo/Library/Mobile Documents/com~apple~CloudDocs/Documents/sdsu/cs577/ultralytics/ultralytics/runs/detect/train/weights/best.pt"
+model_path="/ultralytics/runs/detect/train/weights/best.pt"
 
 model.val(model=model_path, data=data_path, epochs=3)
 
-test_dataset_path="/Users/taonoryo/Library/Mobile Documents/com~apple~CloudDocs/Documents/sdsu/cs577/ultralytics/ultralytics/Coca-cola-Detection-1/test/images"
+test_dataset_path="/ultralytics/Coca-cola-Detection-1/test/images"
 
 model.predict(model=model_path, data=data_path, conf=0.5, source=test_dataset_path, save=True)
 
